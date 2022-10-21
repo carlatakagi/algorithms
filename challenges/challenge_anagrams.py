7034b66e8b15eaf97b90475cfafe415527970392
carla <carlatakagi@gmail.com>
2022-10-21 13:01:09 +0200

    feat: add req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,15 +1,15 @@
 def is_anagram(first_string, second_string):
     if (
         not first_string or not second_string
-        or len(first_string) != len(second_string)
+        or length(first_string) != length(second_string)
     ):
         return False
 
     first, second = list(first_string.lower()), list(second_string.lower())
 
-    for idx in first:
-        if idx in second:
-            second.remove(idx)
+    for index in first:
+        if index in second:
+            second.remove(index)
         else:
             return False
     return True
