efd26cf98f9c9b59cb74189a2a195070db5283cf
carla <carlatakagi@gmail.com>
2022-10-21 12:43:56 +0200

    feat: add req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if len(word) == 0:
+        return False
+
+    if high_index == 0:
+        return True
+    else:
+        if word[low_index].upper() != word[high_index].upper():
+            return False
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
