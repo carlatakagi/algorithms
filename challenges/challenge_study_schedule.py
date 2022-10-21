5f78b54382fb2dc546deb7b6c773c4b24ed98356
carla <carlatakagi@gmail.com>
2022-10-21 12:39:34 +0200

    feat: add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    counter = 0
+    for index in permanence_period:
+        try:
+            if target_time >= index[0] and target_time <= index[1]:
+                counter += 1
+        except TypeError:
+            return None
+    return counter
