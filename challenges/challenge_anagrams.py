7a45ce274219e14a71f90544b9fd0475f18e054b
carla <carlatakagi@gmail.com>
2022-10-21 13:00:06 +0200

    feat: add req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui. commit"""
+    if (
+        not first_string or not second_string
+        or len(first_string) != len(second_string)
+    ):
+        return False
+
+    first, second = list(first_string.lower()), list(second_string.lower())
+
+    for idx in first:
+        if idx in second:
+            second.remove(idx)
+        else:
+            return False
+    return True
